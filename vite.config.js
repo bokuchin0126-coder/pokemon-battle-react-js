a74b2a0988a2ae4bbd4b8dd5f5c2fabc42a56ec4
github user name <github@example.com>
2026-04-01 06:16:51 +0900

    fix: deploy for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-batle-rebuild/",
+  base: "/react-battle-rebuild/",
   plugins: [react()],
 })
